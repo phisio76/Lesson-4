13d79ba82974558689096d0d7c60f951429e4c81
phisio76 <83365774+phisio76@users.noreply.github.com>
2021-05-10 02:01:44 +0300

    Add files via upload
    Lesson 4

---
+++ Array/Program.cs
@@ -0,0 +1,53 @@
+﻿using System; // Павлов Д. 1) Дан  целочисленный  массив  из 20 элементов.  Элементы  массива  могут принимать  целые  значения  от –10 000 до 10 000 включительно. Заполнить случайными числами.  Написать программу, позволяющую найти и вывести количество пар элементов массива, в которых только одно число делится на 3. 
+namespace ArrayClass
+{
+    class MyArray
+    {
+        int[] a;
+        //  Создание массива и заполнение его одним значением el
+        public MyArray(int n, int el)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = el;
+        }
+        //  Создание массива и заполнение его случайными числами от min до max
+        public MyArray(int n, int min, int max) // n- длинна массива
+        {
+            a = new int[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+                a[i] = rnd.Next(min, max);
+        }
+        //Метод подсчета пар чисел, которые делятся на 3
+        public int Pair
+        {
+            get
+            {
+                int count = 0;
+                for (int i = 0; i < a.Length - 1; i++)
+                {
+                    if (a[i] % 3 == 0 | a[i + 1] % 3 == 0)
+                        count++;
+                }
+                return count;
+            }
+        }
+        public override string ToString() //Метод вывода массива на консоль
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + " ";
+            return s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray a = new MyArray(20, -10000, 10000);
+            Console.WriteLine(a.ToString());
+            Console.WriteLine(a.Pair);
+        }
+    }
+}
---
+++ Lesson 4.2/Program.cs
@@ -0,0 +1,71 @@
+﻿using System; // Павлов. Д. 3) Дописать класс для работы с одномерным массивом. Реализовать конструктор, создающий массив определенного размера и заполняющий массив числами от начального значения с заданным шагом. Создать свойство Sum, которое возвращает сумму элементов массива, метод Inverse, возвращающий новый массив с измененными знаками у всех элементов массива (старый массив, остается без изменений),  метод Multi, умножающий каждый элемент массива на определённое число, свойство MaxCount, возвращающее количество максимальных элементов.
+namespace ArrayClass
+{
+    class MyArray
+    {
+        int[] a;
+        //  Создание массива и заполнение его одним значением el
+        public MyArray(int n, int el)
+        {
+            a = new int[n];
+            for (int i = 0; i < n; i++)
+                a[i] = el;
+        }
+        //  Заполнение массивa числами от начального значения с заданным шагом.
+        public MyArray(int n, int min, int max) // n- длинна массива, min-начало, max-шаг
+        {
+            a = new int[n];
+            
+            for (int i = 0; i < n; i++)
+            {
+                a[i] = min + (max * i);
+            }
+        }
+        public void Inverse()
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] *= -1;
+            }
+        }
+        public int Sum()// возвращает сумму элементов массива
+        {
+            int s = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                s += a[i];
+            }
+            return s;
+        }
+        public void Multi(int mlt)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] *= mlt;
+            }
+        }
+        public override string ToString() //Метод вывода массива на консоль
+        {
+            string s = "";
+            foreach (int v in a)
+                s = s + v + " ";
+            return s;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArray a = new MyArray(10, 5, 2);
+            Console.WriteLine("Массив");
+            Console.WriteLine(a.ToString());
+            Console.WriteLine("Сумма чисел в массиве: " + a.Sum());
+            a.Inverse();
+            Console.WriteLine("Инверcия массивa:");
+            Console.WriteLine(a.ToString());
+            a.Multi(5);
+            Console.WriteLine($"Умножение инвертированного массива на 5: {a}");
+        }
+    }
+}
